6246b233997b3b1275c96fefb113afee65001ea2
Slawomir Johansen <baran@fastmail.com>
2022-04-15 00:10:02 +0200

    CHANGE update to react18

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,14 +2,26 @@ import styles from './List.module.scss';
 import Column from '../Column/Column';
 
 const List = () => {
+
+ const columns = [
+  	{ id: 1, title: 'Books', icon: 'book' },
+    { id: 2, title: 'Movies', icon: 'film' },
+    { id: 3, title: 'Games', icon: 'gamepad' }
+  ];
+
+  setTimeout(() => {
+    columns.push({ id: 4, title: 'Test column'});
+    console.log(columns)
+  }, 2000);
+
+
+
   return (
     <div className={styles.list}>
       <header className={styles.header}>Things to do<span>soon</span></header>
       <p className={styles.description}>Interesting things I want to check out</p>
       <section className={styles.columns}>
-        <Column title="Books" icon="book" />
-        <Column title="Movies" icon="film" />
-        <Column title="Games" icon="gamepad" />
+        {columns.map(column => <Column key={column.id} title={column.title} icon={column.icon} /> )}
 
       </section>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles/normalize.scss';
 import './styles/global.scss';
 import 'font-awesome/css/font-awesome.min.css';
 
-ReactDOM.render(
-  <App />,
-  document.querySelector('#root')
-);
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+  <App />
+  );
