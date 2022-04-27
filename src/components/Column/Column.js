9e7559beb49e14f2457232b979c046270692ed3e
Slawomir Johansen <baran@fastmail.com>
2022-04-27 17:36:12 +0200

    CHANGE data management changed to redux

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,8 +1,11 @@
 import styles from './Column.module.scss'
 import Card from '../Card/Card'
 import CardForm from '../CardForm/CardForm'
+import { useSelector } from 'react-redux';
 
 const Column = props => {
+const cards = useSelector((state) => state.cards).filter(card => card.columnId === props.id);
+
   return (
     <article className={styles.column}>
       <h1 className={styles.title}>
@@ -10,7 +13,7 @@ const Column = props => {
         {props.title}
       </h1>
       <ul className={styles.cards}>
-        {props.cards.map(card => <Card key={card.id} title={card.title} />)}
+        {cards.map(card => <Card key={card.id} title={card.title} columnId={card.columnId} />)}
       </ul>
       <CardForm columnId={props.id} action={props.addCard} />
     </article>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import App from './App';
 import './styles/normalize.scss';
 import './styles/global.scss';
 import 'font-awesome/css/font-awesome.min.css';
+import React from 'react';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-  <App />
-  );
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
