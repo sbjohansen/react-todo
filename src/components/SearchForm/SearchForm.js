b5efff7bf31e713b95bb5e3e10d54f74cb0deabd
Slawomir Johansen <baran@fastmail.com>
2022-04-29 20:13:29 +0200

    FIX searchForm dispatch

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -3,7 +3,7 @@ import TextInput from '../TextInput/TextInput';
 import Button from '../Button/Button';
 import { useDispatch } from 'react-redux';
 import { useState } from 'react';
-import { updateSearchString } from '../../redux/store'
+import { updateSearchString } from '../../redux/store';
 
 const SearchForm = () => {
   const dispatch = useDispatch();
@@ -12,7 +12,7 @@ const SearchForm = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(updateSearchString({ searchString }));
+    dispatch(updateSearchString(searchString));
 
     //console.log(searchString)
   };
--- src/redux/store.js
+++ src/redux/store.js
@@ -21,7 +21,7 @@ const reducer = (state, action) => {
     case 'ADD_CARD':
       return { ...state, cards: [...state.cards, { ...action.payload, id: shortid() }] };
     case 'UPDATE_SEARCHSTRING':
-      return { ...state, searchString: action.searchString };
+      return { ...state, searchString: action.payload };
     default:
       return state;
   }
